828da964f37c942b1ba66a2a33f7c1ed631c09a8
unknown <112609769+pankhuri813@users.noreply.github.com>
2023-04-04 15:12:46 +0530

    changes done

---
+++ frontend/src/components/Homepage/Homepage.js
@@ -0,0 +1,75 @@
+import "./Homepage.css";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="navbar">
+        <div className="about">{`About `}</div>
+        <div className="about">Contact</div>
+        <div className="playlistiy">Playlistiy</div>
+        <img className="love-icon" alt="" src="/love@2x.png" />
+        <img className="user-icon" alt="" src="/user@2x.png" />
+      </div>
+      <div className="homepage-child" />
+      <img className="homepage-item" alt="" src="/rectangle-9@2x.png" />
+      <img className="homepage-inner" alt="" src="/rectangle-11@2x.png" />
+      <div className="rectangle-div" />
+      <div className="it-is-a">It is a long established fact that a</div>
+      <div className="homepage-child1" />
+      <div className="homepage-child2" />
+      <div className="it-is-long">It is long established</div>
+      <div className="it-is-a1">It is a long established fact</div>
+      <div className="steps">
+        <div className="steps-child" />
+        <div className="text" />
+        <div className="lorem-ipsum-has" />
+        <div className="steps-item" />
+        <div className="lorem-ipsum-has-container">
+          <p className="industrys-standard-dummy">Lorem Ipsum has been the</p>
+          <p className="industrys-standard-dummy"> industry's standard dummy</p>
+          <p className="industrys-standard-dummy">{` text ever since the 1500s, `}</p>
+          <p className="industrys-standard-dummy">when an unknown printer</p>
+          <p className="industrys-standard-dummy"> took a galley of type and</p>
+          <p className="industrys-standard-dummy">
+            {" "}
+            scrambled it to make a type
+          </p>
+          <p className="industrys-standard-dummy"> specimen book.</p>
+        </div>
+        <div className="steps-inner" />
+        <img className="ellipse-icon" alt="" src="/ellipse-1.svg" />
+        <img className="steps-child1" alt="" src="/ellipse-1.svg" />
+        <div className="div">3</div>
+        <img className="steps-child2" alt="" src="/ellipse-1.svg" />
+        <div className="div1" />
+        <div className="div2">2</div>
+        <div className="lorem-ipsum-has-container1">
+          <p className="industrys-standard-dummy">Lorem Ipsum has been the</p>
+          <p className="industrys-standard-dummy"> industry's standard dummy</p>
+          <p className="industrys-standard-dummy">{` text ever since the 1500s, `}</p>
+          <p className="industrys-standard-dummy">when an unknown printer</p>
+          <p className="industrys-standard-dummy"> took a galley of type and</p>
+          <p className="industrys-standard-dummy">
+            {" "}
+            scrambled it to make a type
+          </p>
+          <p className="industrys-standard-dummy"> specimen book.</p>
+        </div>
+      </div>
+      <div className="types">
+        <div className="browse-the-categories">Browse The Categories</div>
+        <img className="types-child" alt="" src="/rectangle-14@2x.png" />
+        <img className="types-item" alt="" src="/rectangle-15@2x.png" />
+        <img className="types-inner" alt="" src="/rectangle-16@2x.png" />
+        <div className="types-child1" />
+        <div className="types-child2" />
+        <div className="types-child3" />
+        <div className="dance">{`DANCE `}</div>
+        <div className="cooking">COOKING</div>
+        <div className="drawing">DRAWING</div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
