0dfdde0bce5b663e225becc88fa53d5eae50aef6
unknown <112609769+pankhuri813@users.noreply.github.com>
2023-04-11 10:58:44 +0530

    auth0 added

--- frontend/src/components/Homepage/Homepage.js
+++ frontend/src/components/Homepage/Homepage.js
@@ -1,8 +1,11 @@
 import "./Homepage.css";
+import Profile from "../Auth0/Profile";
+import LogoutButton from "../Auth0/Logout";
 
 const Homepage = () => {
   return (
     <div className="homepage">
+      <Profile />
       
       <div className="homepage-child" />
       <div className="steps-1" />
@@ -35,6 +38,8 @@ const Homepage = () => {
          <div className="circle-2"></div>
          <div className="process-3"></div>
          <div className="circle-3"></div>
+         <LogoutButton />
+         <Profile/>
     </div>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
 
-
+import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
- 
+    <Auth0Provider
+    domain="playlistify.jp.auth0.com"
+    clientId="naVxkso0hX6mMeahUlT8KJpfH6YNU4UZ"
+    authorizationParams={{
+      redirect_uri: window.location.origin+'/homepage'
+    }}
+  >
+     <BrowserRouter>
+     <App />
+     </BrowserRouter>
+     </Auth0Provider>
 );
-
-
