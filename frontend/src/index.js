db38a82bd092cce4a3a08628614bf1e7d07dd4e2
unknown <112609769+pankhuri813@users.noreply.github.com>
2023-04-11 11:56:45 +0530

    added the .env file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import { BrowserRouter } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = process.env.DOMAIN;
+const clientId = process.env.CLIENT_ID;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Auth0Provider
-    domain="playlistify.jp.auth0.com"
-    clientId="naVxkso0hX6mMeahUlT8KJpfH6YNU4UZ"
+    domain ={domain}
+    clientId={clientId}
     authorizationParams={{
       redirect_uri: window.location.origin+'/homepage'
     }}
