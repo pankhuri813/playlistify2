945bd0e37707669e263699d0ebbfdce034fe300b
unknown <112609769+pankhuri813@users.noreply.github.com>
2023-04-20 14:12:01 +0530

    Implemented routing

---
+++ frontend/src/components/Category/CategoryDrawing.js
@@ -0,0 +1,23 @@
+import "./Category.css";
+import { Link } from "react-router-dom";
+
+const CategoryPage = () => {
+  return (
+    <div className="categorypage">
+      
+      <div className="dance-categories">Drawing- Categories</div>
+      <div className="categorypage-child" />
+      <div className="categorypage-item" />
+      <div className="categorypage-inner" />
+      <div className="rectangle-div" />
+      <div className="categorypage-child1" />
+      <Link to = '/architecture'><img className="rectangle-icon" alt="" src="../Images/architecture.jpg" /> </Link>
+      <Link to = '/caricature'>  <img className="categorypage-child2" alt="" src="../Images/caricature.jpeg" /> </Link> 
+      < Link to= '/cartoon' > <img className="categorypage-child3" alt="" src="../Images/cartoon.jpeg" /> </Link>
+      < Link to= '/potrait' > <img className="categorypage-child4" alt="" src="../Images/potrait.jpeg"/> </Link>
+      < Link to= '/acrylic' > <img className="categorypage-child5" alt="" src="../Images/acrylic.jpg" /> </Link>
+    </div>
+  );
+};
+
+export default CategoryPage;
--- frontend/src/components/Homepage/Homepage.js
+++ frontend/src/components/Homepage/Homepage.js
@@ -1,7 +1,6 @@
 import "./Homepage.css";
-import Profile from "../Auth0/Profile";
 import LogoutButton from "../Auth0/Logout";
-import Navbar from "../Navbar and footer/Navbar";
+import Navbar from "../Navbar/Navbar";
 import { Link } from "react-router-dom";
 
 const Homepage = () => {
@@ -11,37 +10,38 @@ const Homepage = () => {
       
       <div className="homepage-child" />
       <div className="steps-1" />
-      <div className="heading-homepage3">It is a long established fact</div>
+      <div className="heading-homepage3">“Teaching in the Internet age </div>
       <img className="homepage-item" alt="" src="./Images/background.jpg" />
       <img className="homepage-inner" alt="" src="./Images/laptop.jpg" />
       
       
       <div className="homepage-child1" />
-      <div className="heading-homepage">It is a long established fact that a</div>
+      <div className="heading-homepage">means we must teach</div>
       <div className="homepage-child2" />
-      <div className="heading-homepage2">It is long established</div>
+      <div className="heading-homepage2">tomorrow's skills today</div>
       
       <div className="types">
         <div className="browse-the-categories">Browse The Categories</div>
-       <Link to ='./dance'> <img className="types-child" alt="" src="./Images/dance.jpeg" /> </Link>
-        <Link to ='./cooking'> <img className="types-item" alt="" src="./Images/cooking.jpeg" /> </Link>
-        <Link to ='./drawing'> <img className="types-inner" alt="" src="./Images/drawing.jpeg" /> </Link>
+       <Link to ='/dance'> <img className="types-child" alt="" src="./Images/dance.jpeg" /> </Link>
+        <Link to ='/cooking'> <img className="types-item" alt="" src="./Images/cooking.jpeg" /> </Link>
+        <Link to ='/drawing'> <img className="types-inner" alt="" src="./Images/drawing.jpeg" /> </Link>
         <div className="types-child1" />
         <div className="types-child2" />
         <div className="types-child3" />
         <div className="dance"></div>
-        <div className="cooking">COOKING</div>
-        <div className="drawing">DRAWING</div>
+        <div className="cooking">DRAWING</div>
+        <div className="drawing">COOKING</div>
         <div className="dance-text">DANCE </div>
       </div>
         <div className="process-1"></div>
-        <div className="circle-1"></div>
-         <div className="process-2"> </div>
+        <div className="circle-1"><p className="one"> 1 </p> </div>
+         <div className="process-2">
+         </div>
          <div className="circle-2"></div>
          <div className="process-3"></div>
          <div className="circle-3"></div>
          <LogoutButton />
-         <Profile/>
+         
     </div>
   );
 };
