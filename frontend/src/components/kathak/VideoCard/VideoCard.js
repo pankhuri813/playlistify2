f2508e7be491821659cb78feec75ff799a32f97b
unknown <112609769+pankhuri813@users.noreply.github.com>
2023-04-27 09:29:07 +0530

    added css to videocard in categories

--- frontend/src/components/kathak/VideoCard/VideoCard.js
+++ frontend/src/components/kathak/VideoCard/VideoCard.js
@@ -2,23 +2,23 @@ import React from "react";
 import "./VideoCard.css";
 
 export default function VideoCard(props) {
-  const { title, thumbnail, channel, onDelete } = props;
+  const { title, thumbnail, channel} = props;
 
   return (
-    <div className="main video-card">
+    <div className="display-video-card">
       <div
-        className="video-img"
+        className="video-image"
         style={{ backgroundImage: `url(${thumbnail})` }}
       ></div>
-      <div className="card-body">
-        <h2 className="video-title">{title}</h2>
-        <div className="channel-container">
+      <div className="card-body-display">
+        <h2 className="display-video-title">{title}</h2>
+        <div className="channel-containers">
           <img src="/icons/youtube.svg" alt="" className="yt-logo" />
           <p className="channel-name">{channel}</p>
         </div>
-        <button className="delete-button" onClick={onDelete}>
+        {/* <button className="delete-button" onClick={onDelete}>
           Delete
-        </button>
+        </button> */}
       </div>
     </div>
   );
